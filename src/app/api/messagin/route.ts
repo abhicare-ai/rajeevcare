c2a2303b987bf1dfc13d2e70edb0df63395f0392
drrajeevkumar73 <abhihomeohall72@gmail.com>
2025-06-12 16:44:19 +0530

    j

--- src/app/api/messagin/route.ts
+++ src/app/api/messagin/route.ts
@@ -1,27 +1,4 @@
-// import { NextRequest } from "next/server";
-// import twilio from "twilio";
 
-// const accountSid = process.env.TWILO_ACCOUNT_SID;
-// const authToken = process.env.TWILO_AUTH_TOKEN;
-// const client = twilio(accountSid, authToken);
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { inpute } = await req.json();
-
-//     const message = await client.messages.create({
-//       body: `Here is your link:\n${inpute}`,
-
-//       from: "whatsapp:+14155238886",
-//       to: "whatsapp:+919263049994",
-//     });
-
-//     return Response.json(message);
-//   } catch (err) {
-//     console.error("Error sending message", err);
-//     return Response.json({ error: "Message not sent" }, { status: 500 });
-//   }
-// }
 
 import { NextRequest } from "next/server";
 import nodemailer from "nodemailer";
--- src/app/api/sentocounter/route.ts
+++ src/app/api/sentocounter/route.ts
@@ -1,31 +1,3 @@
-// import { NextRequest } from "next/server";
-// import twilio from "twilio";
-
-// const accountSid = process.env.TWILO_Counter_ACCOUNT_SID;
-// const authToken = process.env.TWILO_Counter_AUTH_TOKEN;
-// const client = twilio(accountSid, authToken);
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { inpute } = await req.json();
-
-//     const message = await client.messages.create({
-//       body: `Here is your link:\n${inpute}`,
-
-//       from: "whatsapp:+14155238886",
-//       to: "whatsapp:+919234942190",
-//     });
-
-//     return Response.json(message);
-//   } catch (err) {
-//     console.error("Error sending message", err);
-//     return Response.json({ error: "Message not sent" }, { status: 500 });
-//   }
-// }
-
-
-
-
 import { NextRequest } from "next/server";
 import nodemailer from "nodemailer";
 import { render } from "@react-email/components";
