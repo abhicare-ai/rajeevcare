d6cdc4b83c935f661c67882bb0a5eb0569af35c8
drrajeevkumar73 <abhihomeohall72@gmail.com>
2025-06-25 13:44:47 +0530

    f

--- src/components/clinic/appointmets/editor/actions.ts
+++ src/components/clinic/appointmets/editor/actions.ts
@@ -23,43 +23,15 @@ export async function appoinment(input: CreateAppointmentSchemaValues) {
   const { user } = await validateRequest();
   if (!user) throw Error("User not found");
 
-  // ✅ Check if appointment already exists for this phone number
-  const existingAppointment = await prisma.appointment.findFirst({
-    where: {
-      phoneNumber: {
-        equals: phoneNumber,
-        mode: "insensitive",
-      },
-      appointmentDate: appointmentDate,
-    },
+
+  // ✅ Get latest token for this user
+  const latest = await prisma.appointment.findFirst({
+    where: { userId: user.id },
+    orderBy: { tokenNo: "desc" },
+    select: { tokenNo: true },
   });
 
-  if (existingAppointment) {
-    return { message: "Appointment already exists." };
-  }
-
-  // ✅ Get the start and end of the appointment day
-  const startOfDay = new Date(appointmentDate);
-  startOfDay.setHours(0, 0, 0, 0);
-  const endOfDay = new Date(appointmentDate);
-  endOfDay.setHours(23, 59, 59, 999);
-
-  // ✅ Find max token for the same appointmentDate
-  const maxTokenToday = await prisma.appointment.aggregate({
-    _max: {
-      tokenNo: true,
-    },
-    where: {
-      appointmentDate: {
-        gte: startOfDay,
-        lte: endOfDay,
-      },
-    },
-  });
-
-  const newTokenNo = maxTokenToday._max.tokenNo
-    ? String(Number(maxTokenToday._max.tokenNo) + 1)
-    : "1";
+  const newTokenNo = (Number(latest?.tokenNo) || 0) + 1;
 
   // ✅ Create new appointment
   const appoinmentData = await prisma.appointment.create({
