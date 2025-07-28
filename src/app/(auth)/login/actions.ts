8e62016fdaa8f4b759be4909444215a40abfa0ac
drrajeevkumar73 <abhihomeohall72@gmail.com>
2025-07-29 01:10:03 +0530

    final

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -16,82 +16,82 @@ export async function login(
   try {
     const { email, branch, password } = loginSchema.parse(credential);
 
-    // //login...........
-    // const existingUser = await prisma.user.findFirst({
-    //   where: {
-    //     email: {
-    //       equals: email,
-    //       mode: "insensitive",
-    //     },
-    //   },
-    // });
+    //login...........
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
 
-    // if (!existingUser || !existingUser.branch || !existingUser.passwordHash) {
-    //   return {
-    //     error: "Incorreact email or password or branch",
-    //   };
-    // }
+    if (!existingUser || !existingUser.branch || !existingUser.passwordHash) {
+      return {
+        error: "Incorreact email or password or branch",
+      };
+    }
 
-    // const existingBranch = existingUser.branch === branch;
+    const existingBranch = existingUser.branch === branch;
 
-    // if (!existingBranch) {
-    //   return {
-    //     error: "Incorreact email or password or branch",
-    //   };
-    // }
+    if (!existingBranch) {
+      return {
+        error: "Incorreact email or password or branch",
+      };
+    }
 
-    // const validPassword = await verify(existingUser.passwordHash, password, {
-    //   memoryCost: 19456,
-    //   timeCost: 2,
-    //   outputLen: 32,
-    //   parallelism: 1,
-    // });
+    const validPassword = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
 
-    // if (!validPassword) {
-    //   return {
-    //     error: "Incorreact email or password or branch",
-    //   };
-    // }
+    if (!validPassword) {
+      return {
+        error: "Incorreact email or password or branch",
+      };
+    }
 
-    // const session = await lucia.createSession(existingUser.id, {});
-    // const sessionCookie = lucia.createSessionCookie(session.id);
-    // (await cookies()).set(
-    //   sessionCookie.name,
-    //   sessionCookie.value,
-    //   sessionCookie.attributes,
-    // );
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    (await cookies()).set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
 
-    // return redirect("/");
+    return redirect("/");
 
     //signup....
 
-      const passwordHash = await hash(password, {
-        memoryCost: 19456,
-        timeCost: 2,
-        outputLen: 32,
-        parallelism: 1,
-      });
+    //   const passwordHash = await hash(password, {
+    //     memoryCost: 19456,
+    //     timeCost: 2,
+    //     outputLen: 32,
+    //     parallelism: 1,
+    //   });
 
-      const userId = generateIdFromEntropySize(10);
+    //   const userId = generateIdFromEntropySize(10);
 
-     await prisma.user.create({
-        data: {
-          id: userId,
-          branch,
-          email,
-          passwordHash,
-        },
-      });
+    //  await prisma.user.create({
+    //     data: {
+    //       id: userId,
+    //       branch,
+    //       email,
+    //       passwordHash,
+    //     },
+    //   });
 
-      const session = await lucia.createSession(userId, {});
-      const sessionCookie = lucia.createSessionCookie(session.id);
-      (await cookies()).set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes,
-      );
+    //   const session = await lucia.createSession(userId, {});
+    //   const sessionCookie = lucia.createSessionCookie(session.id);
+    //   (await cookies()).set(
+    //     sessionCookie.name,
+    //     sessionCookie.value,
+    //     sessionCookie.attributes,
+    //   );
 
-      return redirect("/");
+    //   return redirect("/");
   } catch (error) {
     if (isRedirectError(error)) throw error;
     console.error(error);
